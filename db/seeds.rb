13b3ab2f640f5d93d3c77a7a9d52be36c1584826
mateomh <mmojicah.84@gmail.com>
2020-12-07 15:35:43 -0500

    Created the seed data for the inital values for the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create({name: 'mateo'})
+
+Category.create([{name: 'Work'}, {name: 'Exercise'}, {name: 'Commute'}])
+Category.create([{name: 'Side Projects'}, {name: 'Learning'}, {name: 'Sleep'}])
+Category.create([{name: 'Entertainment'}])
+
+valid_user = User.first
+valid_cat = Category.first
+
+Task.create({desc: 'Test Task',time: 2.5, user_id: valid_user.id, category_id: valid_cat.id})
