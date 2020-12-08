094f98d2bc61abaaaa550ebd5abcd1547079e67d
mateomh <mmojicah.84@gmail.com>
2020-12-08 15:55:41 -0500

    Add tests for the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it '1. Checks validations for precense of the name field' do
+    u = User.create(name: nil)
+    expect(u).to be_invalid
+  end
+
+  it '2. Checks validations for uniqueness of the name field' do
+    User.create(name: 'testy')
+    u = User.create(name: 'testy')
+    expect(u).to be_invalid
+  end
+
+  it '3. Checks the task model association' do
+    User.create(name: 'testy')
+    test_user = User.first
+    Category.create(name: 'test group')
+    test_category = Category.first
+
+    Task.create({user_id: test_user.id, category_id: test_category.id, desc: 'RSPEC Task', time: 2.5})
+    test_task = test_user.tasks.first
+    
+    expect(test_task.desc).to eql('RSPEC Task') #be 'RSPEC Task'
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,10 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  
+  # Added by ME verbose configuration
+  config.formatter = :documentation
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
@@ -91,4 +95,5 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
+
 end
