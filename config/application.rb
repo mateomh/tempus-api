956e4b033ae4ec5dc409ebaa004e7c8590c5d699
mateomh <mmojicah.84@gmail.com>
2020-12-08 15:26:43 -0500

    Add rack-corrs gem to the project and configuration in the config/application.rb file

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,13 @@ module TempusApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Added by ME RackCors configuration
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :delete]
+      end
+    end
   end
 end
