0bad2eb8e702d7788591d56115351ab702ee6f65
mateomh <mmojicah.84@gmail.com>
2020-12-16 14:40:41 -0500

    Add error code response when no user is found and remover all istance variables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,50 +1,35 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show update destroy]
-
   # GET /users
   def index
-    @users = User.all
+    users = User.all
 
-    render json: @users
+    render json: users
   end
 
   # GET /users/1
   def show
-    render json: @user
+    user = User.where(name: params[:id])
+    
+    if user.empty?
+      render response: 'User not found', status: 404
+    else
+      render json: user
+    end
+    
   end
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if user.save
+      render json: user, status: :created, location: user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
-  end
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    # @user = User.find(params[:id])
-    @user = User.where(name: params[:id])
-  end
 
   # Only allow a trusted parameter "white list" through.
   def user_params
