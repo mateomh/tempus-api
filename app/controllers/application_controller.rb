5d5b745e9487be6656cd139b5bb5a63f8f8e89a8
mateomh <mmojicah.84@gmail.com>
2020-12-17 11:55:31 -0500

    Fix bug in the authentication method in the applications controller involving the extracting of the user data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::API
   def check_authorization
     token = request.headers['token']
-    puts request.headers['token']
-    puts '&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
 
     begin
       decoded_token = JWT.decode(
@@ -10,24 +8,15 @@ class ApplicationController < ActionController::API
         Rails.application.secrets.secret_key_base,
         'HS256'
       )
-      data = decoded_token[0][0]
-
+      data = decoded_token[0]
+      
       user_id = data['id']
-
+      
       user = User.find(user_id)
     rescue StandardError
       user = nil
     end
-
-    # data = decoded_token[0][0]
-
-    # user_id = data['id']
-
-    # user = User.find(user_id)
-
-    # user.as_json
+    
     user
-    # puts decoded_token
-    # puts '#######################################3'
   end
 end
